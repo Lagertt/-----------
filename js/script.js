1d37f68fd9c0bb67a9a3884fec445857b9915629
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-03 16:15:50 +0500

    add smooth scroll

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/////////////////////плавный скрол///////////////////////////////
+const linksHead = document.querySelectorAll('.nav__link');
+
+linksHead.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const ID = event.target.getAttribute('href').substr(1);
+
+    document.getElementById(ID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});
