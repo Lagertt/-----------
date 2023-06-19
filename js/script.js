eb4e6d0b782288f870e4f9ddb6d9f3bd8335fbd4
Polina Ryabova <polinaryabova.2@gmail.com>
2023-06-19 17:18:48 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-/////////////////////плавный скрол///////////////////////////////
-const linksHead = document.querySelectorAll('.nav__link');
-
-linksHead.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const ID = event.target.getAttribute('href').substr(1);
-
-    document.getElementById(ID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
-});
-
 /////////////////////раскрытие таблиц///////////////////////////////
 const btnShowMore = document.querySelector('.tables__button');
 
@@ -71,3 +55,19 @@ function MenuAction() {
   menu.classList.toggle('nav__list--active');
   btnBurger.classList.toggle('btn-menu--active');
 }
+
+/////////////////////плавный скрол///////////////////////////////
+const linksHead = document.querySelectorAll('.nav__link');
+
+linksHead.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const ID = event.target.getAttribute('href').substr(1);
+
+    document.getElementById(ID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+});
